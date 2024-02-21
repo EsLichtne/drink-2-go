06a06954adde0081af8603238d227eeaff9e812e
Ксения <easyghoul@gmail.com>
2024-02-21 16:37:04 +0700

    feat: Добавляет функционал слайдера

---
+++ source/scripts/slider.js
@@ -0,0 +1,63 @@
+import {slides} from './slides-mocks';
+
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const title = slider.querySelector('.slider__title');
+const image = slider.querySelector('.slider__image-wrapper');
+const description = slider.querySelector('.slider__description');
+const oldPrice = slider.querySelector('.price--old');
+const price = slider.querySelector('.price--new');
+const backButton = slider.querySelector('.slider__button--back');
+const forwardButton = slider.querySelector('.slider__button--forward');
+const paginationButtons = slider.querySelectorAll('.pagination__button');
+
+let currentSlideIndex = 0;
+
+const updatePaginationButtons = () => {
+  paginationButtons.forEach((button, index) => {
+    if (index === currentSlideIndex) {
+      button.classList.add('pagination__button--current');
+    } else {
+      button.classList.remove('pagination__button--current');
+    }
+  });
+};
+
+const updateSlideContent = (index) => {
+  const slide = slides[index];
+
+  hero.style.setProperty('--product-color', slide.color);
+  title.textContent = slide.title;
+  image.innerHTML = slide.image;
+  description.innerHTML = slide.description;
+  oldPrice.textContent = slide.oldPrice;
+  price.textContent = slide.price;
+};
+
+const getNextSlide = () => {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  updateSlideContent(currentSlideIndex);
+  updatePaginationButtons();
+};
+
+const getPreviousSlide = () => {
+  currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+  updateSlideContent(currentSlideIndex);
+  updatePaginationButtons();
+};
+
+backButton.addEventListener('click', () => {
+  getPreviousSlide();
+});
+
+forwardButton.addEventListener('click', () => {
+  getNextSlide();
+});
+
+paginationButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    currentSlideIndex = index;
+    updateSlideContent(currentSlideIndex);
+    updatePaginationButtons();
+  });
+});
